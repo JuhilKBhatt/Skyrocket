37723a69e2bf9807055655fafc0968f03f190c72
Juhil K Bhatt <100752923+JuhilKBhatt@users.noreply.github.com>
2026-02-15 15:54:04 +1100

    Removed Old Package

--- backend/app/services/trading_bot.py
+++ backend/app/services/trading_bot.py
@@ -2,10 +2,8 @@
 from lumibot.brokers import Alpaca
 from lumibot.backtesting import YahooDataBacktesting
 from lumibot.strategies.strategy import Strategy
-from lumibot.traders import Trader
-from datetime import datetime 
-from alpaca_trade_api import REST 
-from timedelta import Timedelta 
+from datetime import datetime
+from timedelta import Timedelta
 from finbert_utils import estimate_sentiment
 import os
 import requests
@@ -26,7 +24,6 @@ class MLTrader(Strategy):
         self.sleeptime = "24H" 
         self.last_trade = None 
         self.cash_at_risk = cash_at_risk
-        self.api = REST(base_url=BASE_URL, key_id=API_KEY, secret_key=API_SECRET)
 
     def position_sizing(self): 
         cash = self.get_cash() 
